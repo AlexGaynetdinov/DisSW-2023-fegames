2f31fe002ef41dafd87a61cc48a5d52ef2414f0a
AluIsmael <114938688+AluIsmael@users.noreply.github.com>
2023-06-24 13:01:49 +0200

    Add files via upload

---
+++ src/app/games.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GamesService {
+  private ws? : WebSocket
+
+  constructor(private httpClient : HttpClient) { }
+
+  requestGame() {
+    this.httpClient.get<any>("http://localhost/users/register" + sessionStorage.getItem("player"))
+        .subscribe(
+          respuesta => {
+            sessionStorage.setItem("idMatch", respuesta.id)
+            console.log(respuesta)
+            this.prepareWebSocket()
+          },
+            error => {
+            console.log(error)
+          })
+  }
+
+  prepareWebSocket() {
+    this.ws = new WebSocket("ws://localhost/wsGames")
+    this.ws.onopen = function() {
+      console.log("ws abierto")
+    }
+
+    this.ws.onmessage = function(event) {
+      console.log("mensaje recibido: " + JSON.stringify(event.data))
+    }
+
+    this.ws.onclose = function() {
+      console.log("ws cerrado")
+    }
+
+    this.ws.onerror = function(event) {
+      console.log("error en ws: " + JSON.stringify(event))
+    }
+  }
+}
